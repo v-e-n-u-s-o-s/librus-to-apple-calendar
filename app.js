02967ec9fe053f075bf44e7caea718445f3bef20
John <janekpieklo@icloud.com>
2022-01-04 12:59:44 +0100

    Big one

--- app.js
+++ app.js
@@ -2,13 +2,12 @@
 var debug = require('debug')('my express app');
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+    
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -25,7 +24,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,105 @@
 'use strict';
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Librus = require('librus-api');
+const fs = require('fs');
+const ics = require('ics');
 
-/* GET home page. */
-router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
-});
+let client = new Librus();
+let filename = "";
+let file = false;
 
 module.exports = router;
+
+router.get('/', function (req, res) {
+    res.sendFile()
+})
+
+router.get('/calendar', function (req, res) {
+    const current_url = req.protocol + '://' + req.get('host') + "/";
+
+    let login = req.query.login;
+    let password = req.query.password;
+    filename = req.query.filename;
+
+    if (login == "" || password == "" || filename == "") res.end()
+
+    filename += ".ics";
+
+    client.authorize(login, password).then(function () {
+        client.calendar.getTimetable().then(data => { splitDays(JSON.parse(JSON.stringify(data)), filename) });
+    })
+
+    let interval = setInterval(() => { if (file) res.send(current_url + "calendars/" + filename); clearInterval(interval) }, 1000)
+});
+
+
+
+function splitDays(data, filename, ammount = 1) {
+    let day = 0;
+    var callendar =
+`
+BEGIN: VCALENDAR
+VERSION: 2.0
+CALSCALE: GREGORIAN
+PRODID: adamgibbons / ics
+METHOD: PUBLISH
+X - PUBLISHED - TTL: PT1H
+`;
+
+    function getPreviousMonday() {
+        var date = new Date();
+        var day = date.getDay();
+        var prevMonday = new Date();
+        if (date.getDay() == 0) {
+            return date;
+        }
+        else {
+            prevMonday.setDate(date.getDate() - (day - 1));
+        }
+
+        return prevMonday;
+    }
+
+    function saveCalendar(data, filename) {
+        fs.writeFile("./public/calendars/" + filename, data + "END:VCALENDAR", function (err) {
+            if (err) throw err;
+            file = true;
+        });
+    }
+
+    function addEvent(time, day, month, title, teacher) {
+        time = time.split('-')[0].replace(" ", "").split(":")
+        let event = {
+            start: [getPreviousMonday().getUTCFullYear(), Number(month), Number(day), Number(time[0]), Number(time[1])],
+            duration: { minutes: 45 },
+            title: title,
+            categories: ['lesson'],
+            organizer: { name: teacher },
+        }
+
+        return ics.createEvent(event, (error, value) => {
+            if (error) {
+                console.log(error)
+                return
+            }
+            return value.split("\n").slice(6).join("\n").replace(/\r?\n?[^\r\n]*$/, "").replace(/\r?\n?[^\r\n]*$/, "");
+        })
+    }
+
+    Object.keys(data.table).forEach(function (key) {
+        for (let i = 0; i < data.hours.length; i++) {
+            if (data.table[key][i]) {
+                let _temp = data.table[key][i].title.split('-');
+                callendar += addEvent(data.hours[i], day, _temp[0].replace("\n", ""), _temp[1].replace("\n", ""));
+            }
+            else {
+                continue;
+            }
+            callendar += "\n";
+        }
+        day++;
+    })
+
+    saveCalendar(callendar, filename);
+}
